0ec596dd002a69c46f45cabdcf55a71cf6a991a4
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-07 19:33:36 +0600

    Task00 done

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,1 +1,39 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// 0. Напишите программу, которая на вход
+// принимает число и выдает его квадрат
+// (число, умноженное само на себя)
+
+Console.Clear();
+bool condition = true;
+
+//Запускаем цикл
+while (condition)
+{
+    Console.Write("<----- Введите число: ");
+
+    var input = Console.ReadLine();
+
+    //Проверяем входящее значение на null
+    if (input != null)
+    {
+        var str = input.Trim().Split(' ')[0];
+        double output;
+
+        //Проверяем возможность конвертации строки в число
+        if (double.TryParse(str, out output))
+        {
+            int number = Convert.ToInt32(output);
+
+            //Если все ОК, то печатаем ответ и выходим из цикла
+            Console.WriteLine(number * number);
+            condition = false;
+        }
+        else
+        {
+            Console.WriteLine($"Полученное значение -> ({str}) <- не является числом");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ввод не может быть пустым");
+    }
+}
