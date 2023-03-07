be54ff4c13dacc123baed6c9f399c886a1ffd12f
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-07 23:48:43 +0600

    Task06 done

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 6. Напишите программу, которая на вход
+// принимает число и выдает, является ли
+// число четным (делиться ли оно на два без остатка)
+
+// Например:
+// 4 -> да
+// -3 -> нет
+
+Console.Clear();
+
+//Запускаем цикл
+while (true)
+{
+    Console.Write("<----- Введите число: ");
+
+    var input = Console.ReadLine();
+
+    //Проверяем входящее значение на null
+    if (input != null)
+    {
+        var str = input.Trim().Split(' ')[0];
+        double number;
+
+        //Проверяем возможность конвертации строки в число
+        if (double.TryParse(str, out number))
+        {
+            if (number > 0 && number % 2 == 0)
+            {
+                Print("да");
+            }
+            else
+            {
+                Print("нет");
+            }
+            break;
+        }
+        else
+        {
+            Print($"Ошибка: значение ({str}) не удовлетворяет условиям");
+        }
+    }
+    else
+    {
+        Print("Ошибка: значение не может быть пустым");
+    }
+
+    static void Print(string result)
+    {
+        Console.WriteLine(result);
+    }
+}
