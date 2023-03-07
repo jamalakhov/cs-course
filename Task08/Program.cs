ed63d21f8ef95878b7f5bf99c6ea96c2356491d3
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-08 00:09:12 +0600

    Task08 done

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 8. Напишите программу, которая на вход
+// принимает число (N), а на выходе
+// показывает все четные числа от 1 до N
+
+// Например:
+// 5 -> 2, 4
+
+Console.Clear();
+
+//Запускаем цикл
+while (true)
+{
+    Console.Write("<----- Введите целое положительное число: ");
+
+    var input = Console.ReadLine();
+
+    //Проверяем входящее значение на null
+    if (input != null)
+    {
+        var str = input.Trim().Split(' ')[0];
+        double number;
+
+        //Проверяем возможность конвертации строки в число
+        if (double.TryParse(str, out number))
+        {
+            //Проверяем на целое число
+            if (Convert.ToInt32(number) == Convert.ToDouble(number))
+            {
+                int myInt = Convert.ToInt32(number);
+                //Проверяем на положительное число
+                if (myInt > 1)
+                {
+                    //Печатаем числа
+                    // int i =
+                    for (int i = 2; i <= myInt; i += 2)
+                    {
+                        Console.Write($"{i} ");
+                    }
+                    //Завершаем цикл
+                    break;
+                }
+                else
+                {
+                    Print($"Ошибка: число -> ({myInt}) меньше или равно 1");
+                }
+            }
+            else
+            {
+                Print($"Ошибка: дробное число ({number})");
+            }
+        }
+        else
+        {
+            Print($"Ошибка: не является числом -> ({str})");
+        }
+    }
+    else
+    {
+        Print("Ошибка: значение не может быть пустым");
+    }
+
+    static void Print(string result)
+    {
+        Console.WriteLine(result);
+    }
+}
