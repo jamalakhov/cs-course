c7af8de7e9565fdf954a5056e9c4204d6e73a2ab
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-07 22:17:11 +0600

    Task07 done

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 7. Напишите программу, которая на вход
+// принимает трехзначное число и на выходе
+// показывает последнюю цифру этого числа
+
+// Например:
+// 456 -> 6
+
+
+Console.Clear();
+
+//Запускаем цикл
+while (true)
+{
+    Console.Write("<----- Введите трехзначное число: ");
+
+    var input = Console.ReadLine();
+
+    //Проверяем входящее значение на null
+    if (input != null)
+    {
+        string str = input.Trim().Split(' ')[0];
+        double number;
+
+        //Проверяем возможность конвертации строки в число
+        if (double.TryParse(str, out number))
+        {
+            // Проверяем на целое число
+            if (Convert.ToInt32(number) == Convert.ToDouble(number))
+            {
+                int myInt = Math.Abs(Convert.ToInt32(number));
+
+                //Проверка на 3-х значное число
+                if (myInt >= 100 && myInt < 1000)
+                {
+                    //Печатаем и выходим из цикла
+                    Console.WriteLine(myInt % 10);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка: не трехзначное число ({number})");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка: дробное число ({number})");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Ошибка: значение ({str}) не является числом");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ошибка: пустой ввод");
+    }
+}
