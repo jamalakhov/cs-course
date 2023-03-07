16db5e0c225843a70dd4425ef635737b65f8fafd
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-07 22:49:24 +0600

    Task02 done

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Напишите программу, которая на вход
+// принимает два числа и выдает большее число
+
+// Например:
+// a = 5, b = 7 -> max = 7
+
+Console.Clear();
+
+bool condition = true;
+
+//Запускаем цикл
+while (condition)
+{
+    bool myBrake = false;
+
+    Console.Write("<----- Введите два числа через пробел: ");
+
+    var input = Console.ReadLine();
+
+    //Проверяем входящее значение на null
+    if (input != null)
+    {
+        string[] str = input.Trim().Split(' ');
+
+        //Проверяем количество введенных значений
+        if (str.Length == 2)
+        {
+            double[] arrayDouble = new double[2];
+
+            //Проверяем возможность конвертации строки в число
+            for (int i = 0; i < str.Length; i++)
+            {
+                double number;
+                if (double.TryParse(str[i], out number))
+                {
+                    arrayDouble[i] = number;
+                }
+                else
+                {
+                    Print($"Ошибка: не является числом -> ({str[i]})");
+                    myBrake = true;
+                    break;
+                }
+            }
+
+            if (myBrake)
+            {
+                continue;
+            }
+
+            //Сравниваем числа
+            if (arrayDouble[0] > arrayDouble[1])
+            {
+                Print($"max = {arrayDouble[0]}");
+            }
+            else
+            {
+                Print($"max = {arrayDouble[1]}");
+            }
+
+            condition = false;
+        }
+        else
+        {
+            Print($"Ошибка: некорректное значение -> ({input})");
+        }
+    }
+    else
+    {
+        Print("Вводимое значение не может быть пустым");
+    }
+
+    static void Print(string resulr)
+    {
+        Console.WriteLine(resulr);
+    }
+}
