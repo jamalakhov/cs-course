6e4e19f17a4ceb0346006b24e599a074f5af06ba
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-10 17:27:16 +0600

    Task09 done

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа
+// 78 -> 8
+// 25 -> 5
+// 93 -> 9
+
+Console.Clear();
+
+int random = new Random().Next(10, 100);
+
+Console.WriteLine($"Случайное число из [10, 99] -> {random}");
+
+int max = MaxDigit(random);
+
+Console.WriteLine(max);
+
+int MaxDigit(int number)
+{
+    int digit1 = number / 10;
+    int digit2 = number % 10;
+
+    return digit1 > digit2 ? digit1 : digit2;
+}
