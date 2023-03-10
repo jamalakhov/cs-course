852a241ed1316e978b2b21a5a1ee2066954c3b19
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-10 17:39:22 +0600

    Task11 done

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Console.Clear();
+
+int random = new Random().Next(100, 1000);
+
+Console.WriteLine($"Случайное число из [100, 999] -> {random}");
+
+int number = Delete2Digit(random);
+
+Console.WriteLine(number);
+
+int Delete2Digit(int number)
+{
+    int digit1 = number / 100;
+    int digit2 = number % 10;
+
+    return digit1 * 10 + digit2;
+}
