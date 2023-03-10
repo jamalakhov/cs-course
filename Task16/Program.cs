441bf30a1f0b7e829c42d7aa523b1a0e52d85dee
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-10 18:26:02 +0600

    Task16 done

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 12. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет
+
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+
+int firstNumber = ReadConsole();
+
+Console.Write("Введите второе число: ");
+
+int secondNumber = ReadConsole();
+
+bool result = IsEqualSquared(firstNumber, secondNumber);
+
+Print(result ? "да" : "нет");
+
+bool IsEqualSquared(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
+
+int ReadConsole()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Print(string text)
+{
+    Console.WriteLine(text);
+}
