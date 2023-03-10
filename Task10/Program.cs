457fb72b4f27df3b871145134dc6a25ec141e18b
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-10 18:48:38 +0600

    Task10 done

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 10. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру
+// этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Clear();
+
+Console.Write("Введите трёхзначное число: ");
+
+int input = ReadConsole();
+
+int result = SecondNumber(input);
+
+Print(Convert.ToString(result));
+
+int SecondNumber(int num)
+{
+    return Math.Abs(num / 10 % 10);
+}
+
+int ReadConsole()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Print(string text)
+{
+    Console.WriteLine(text);
+}
