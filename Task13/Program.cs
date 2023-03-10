4894454cc688cb245fe7531fe84694deaeeeb70b
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-10 19:35:08 +0600

    Task13 done

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 13. Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+
+long input = ReadConsole();
+
+string result = ThirdDigit(input);
+
+Print(result);
+
+string ThirdDigit(long num)
+{
+    if (num < 100)
+    {
+        return "третьей цифры нет";
+    }
+    else
+    {
+        while (num > 999)
+        {
+            num /= 10;
+        }
+
+        return Convert.ToString(num % 10);
+    }
+}
+
+long ReadConsole()
+{
+    return Convert.ToInt64(Console.ReadLine());
+}
+
+void Print(string text)
+{
+    Console.WriteLine(text);
+}
