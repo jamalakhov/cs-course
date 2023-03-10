0b77fa07a835cbdc44eece55b7a72fbac3087ff8
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-10 19:45:02 +0600

    Task15 done

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 15. Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+
+Console.Write("Введите номер дня недели: ");
+
+int input = ReadConsole();
+
+bool weekend = IsWeekend(input);
+
+Console.WriteLine(weekend ? "да" : "нет");
+
+bool IsWeekend(int num)
+{
+    return num == 6 || num == 7;
+}
+
+int ReadConsole()
+{
+    return Convert.ToInt16(Console.ReadLine());
+}
