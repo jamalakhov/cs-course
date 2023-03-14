904900281b6d470f6443fda8493f34ad431646e3
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-14 18:54:36 +0600

    Task17 done

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -1,25 +1,49 @@
-﻿Console.Clear();
+﻿// 17. Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+// номер четверти плоскости, в которой находится эта
+// точка.
 
-Console.WriteLine("Введите координаты точки");
+Console.Clear();
 
-Console.Write("X: ");
-int xCoordinate = Convert.ToInt32(Console.ReadLine());
+int xCoordinate = GetCoordinate("Введите координат x: ");
+int yCoordinate = GetCoordinate("Введите координат y: ");
+int quarter = GetQuarter(xCoordinate, yCoordinate);
 
-Console.Write("Y: ");
-int yCoordinate = Convert.ToInt32(Console.ReadLine());
+PrintResult(quarter);
 
-int quarter = Quarter(xCoordinate, yCoordinate);
-string result = quarter > 0 
-                ? $"Указанные координаты соответствуют четверти -> {quarter}" 
-                : "Введены некорректные коррдинаты";
-
-System.Console.WriteLine(result);
-
-int Quarter(int x, int y)
+//////////////////////////////////////////////////////////
+int GetQuarter(int x, int y)
 {
     if(x > 0 && y > 0) return 1;
     if(x < 0 && y > 0) return 2;
     if(x < 0 && y < 0) return 3;
     if(x > 0 && y < 0) return 4;
     return 0;
+}
+
+int GetCoordinate(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        var input = Console.ReadLine();
+
+        if(Int32.TryParse(input, out int coordinate) && coordinate != 0)
+        {
+            return coordinate;            
+        }
+        else
+        {
+            Console.WriteLine($"Ошибка: введено недопустимое значение ({input})");
+        }        
+    }
+}
+
+void PrintResult(int quarter)
+{
+    string result = quarter > 0 
+                ? $"Указанные координаты соответствуют четверти -> {quarter}"
+                : "Введены некорректные координаты";
+
+    Console.WriteLine(result);
 }
