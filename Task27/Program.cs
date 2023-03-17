6c0ee9a9e8fd331bfd6653b544b2aee555c18efd
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-17 20:00:01 +0600

    Task27 done

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 27. Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+int inputNumber = ReadConsole("Введите число: ");
+int resultSum = GetSumDigit(inputNumber);
+
+Console.WriteLine(resultSum);
+
+int GetSumDigit(int input)
+{
+    int sum = 0;
+
+    while(input > 0)
+    {
+        sum += input % 10;
+        input /= 10;
+    }
+
+    return sum;
+}
+
+int ReadConsole(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        string input = "";
+        try
+        {
+            input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if (number == 0) throw new FormatException();
+            if(number < 0) number *= -1;
+
+            return number;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine($"Ошибка ввода {input}");
+        }
+    }
+}
