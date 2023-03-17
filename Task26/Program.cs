fe854839d5531c48a6c8de0bdf9ea8139c3b7fbb
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-17 18:27:41 +0600

    Task26 done

---
+++ Task26/Program.cs
@@ -0,0 +1,27 @@
+﻿// 26. Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+
+Console.Write("Введите целое число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int countDigit = CountDigit(number);
+
+Console.WriteLine($"Количество цифр в числе {number}: {countDigit}");
+
+int CountDigit(int num)
+{
+    int count = 0;
+
+    while (num != 0)
+    {
+        count++;
+        num /= 10;        
+    }
+
+    return count;
+}
