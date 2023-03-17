385a436f0f0054a3b5cfcf37a54c2784021362f9
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-17 18:31:21 +0600

    Task28 done

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Clear();
+
+Console.Write("Введите целое число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number <= 0)
+{
+    Console.WriteLine($"Ошибка: введено неверное значение {number}");
+    return;
+}
+
+int factorial = Factorial(number);
+
+Console.WriteLine($"Произведение чисел до {number}: {factorial}");
+
+int Factorial(int num)
+{
+    int result = 1;
+
+    for (int i = 2; i <= num; i++)
+    {
+        checked
+        {
+            result *= i;            
+        }        
+    }
+    return result;
+}
