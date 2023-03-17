0135afcd6f74a12a163f0e108a3fa7b29f054b68
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-17 19:34:56 +0600

    Task25 done

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 25. Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243
+// 2, 4 -> 16
+
+Console.Clear();
+
+
+
+int firstNumber = ReadConsole("Введите число: ");
+int secondNumber = ReadSecondNumber("Введите степень: ");
+int result = PowerUp(firstNumber, secondNumber);
+
+Console.WriteLine(result);
+
+
+int PowerUp(int originNum, int powerNum)
+{
+    int subNum = originNum;
+
+    for (int i = 1; i < powerNum; i++)
+    {
+        subNum *= originNum;
+    }
+
+    return subNum;
+}
+
+int ReadConsole(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        string input = "";
+        try
+        {
+            input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if (number == 0) throw new FormatException();
+
+            return number;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine($"Ошибка ввода {input}");
+        }
+    }
+}
+
+int ReadSecondNumber(string message)
+{
+    while (true)
+    {
+        int number = ReadConsole(message);
+        if (number > 0) return number;
+
+        Console.WriteLine($"Число не натуральное -> {number}");
+    }
+}
