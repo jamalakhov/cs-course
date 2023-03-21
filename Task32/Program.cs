54b34db969aaeaf1d9a9a062fd26392b1ecd0402
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-21 19:10:08 +0600

    Task32 done

---
+++ Task32/Program.cs
@@ -0,0 +1,43 @@
+﻿// 32. Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+Console.Clear();
+
+int[] arrayElements = CreateArrayRndInt(12, -6, 7);
+
+Console.WriteLine("Before:");
+PrintArray(arrayElements);
+
+ConvertElementsToArray(arrayElements);
+Console.WriteLine();
+
+Console.WriteLine("After:");
+PrintArray(arrayElements);
+
+void ConvertElementsToArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", arr));
+}
