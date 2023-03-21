6ec05c1bef983f578013e9ec6761be8b2582c906
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-21 19:15:11 +0600

    Task35 done

---
+++ Task35/Program.cs
@@ -0,0 +1,49 @@
+﻿// 35. Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+Console.Clear();
+
+int[] arr = CreateArrayRndInt(5, -10, 30);
+
+int count = CountElements(arr, 10, 99);
+
+Console.WriteLine(count);
+
+PrintArray(arr);
+
+int CountElements(int[] array, int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= min && array[i] <= max)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", arr));
+}
