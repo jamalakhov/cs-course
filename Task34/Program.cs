080933298f1265462ce5b907244554cdec4d428a
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-23 03:50:14 +0600

    Task34 done

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 34. Задайте массив заполненный случайными
+// положительными трёхзначными числами.
+// Напишите программу, которая покажет количество
+// чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int[] myArray = CreateArrayRndInt(12, 111, 333);
+
+PrintArray(myArray);
+
+int result = CountEvenElements(myArray);
+
+Console.WriteLine("\n" + result);
+
+
+int CountEvenElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", arr));
+}
