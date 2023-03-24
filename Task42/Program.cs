1305b6b4c19b25b4b1f98886cd46ed49cf15f2dc
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-24 17:26:52 +0600

    Task42 done

---
+++ Task42/Program.cs
@@ -0,0 +1,47 @@
+﻿// 42. Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 46 -> 101110
+// 3 -> 11
+// 2 -> 10
+
+Console.Clear();
+
+int decimal1 = 2;
+int result = DecToBinInt(decimal1);
+
+Console.WriteLine(result);
+
+string DecToBinString(int decimalNumber)
+{
+    if (decimalNumber == 0 || decimalNumber == 1)
+    {
+        return decimalNumber.ToString();
+    }
+
+    string dec = string.Empty;
+    while (decimalNumber > 0)
+    {
+        dec = decimalNumber % 2 + dec;
+        decimalNumber /= 2;
+    }
+    return dec;
+}
+
+int DecToBinInt(int decimalNumber)
+{
+    if (decimalNumber == 0 || decimalNumber == 1)
+    {
+        return decimalNumber;
+    }
+
+    int dec = 0;
+    int i = 1;
+    while (decimalNumber > 0)
+    {
+        dec = dec + decimalNumber % 2 * i;
+        decimalNumber /= 2;
+        i *= 10;
+    }
+    return dec;
+}
