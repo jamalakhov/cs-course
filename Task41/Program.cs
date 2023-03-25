ec7e6e74d7bca0d3af718025d717493fedc7b1f2
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-25 10:57:15 +0600

    Task41 done

---
+++ Task41/Program.cs
@@ -0,0 +1,67 @@
+﻿// 41. Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223 -> 3
+Console.Clear();
+
+int numb = 5;
+
+double[] myArr = ReadConsole($"Введите {numb} чисел", numb);
+
+int countResult = CountDigit(myArr);
+
+Console.WriteLine(countResult);
+
+//////////////////////////////////////////////////////////////
+
+int CountDigit(double[] arrNumb)
+{
+    int count = 0;
+    for (int i = 0; i < arrNumb.Length; i++)
+    {
+        if (arrNumb[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+double[] ReadConsole(string message, int countNumber)
+{
+    Console.WriteLine(message + "\n");
+
+    int nextNumber = countNumber - countNumber + 1;
+
+    double[] arrayNumber = new double[countNumber];
+
+    while (countNumber > 0)
+    {
+        while (true)
+        {
+            Console.Write($"Введите {nextNumber} число: ");
+
+            var input = Console.ReadLine();
+            if (input != null)
+            {
+                double number;
+                if (double.TryParse(input, out number))
+                {
+                    arrayNumber[nextNumber - 1] = number;
+                    nextNumber++;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка: значение ({input}) не является числом");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: пустой ввод");
+            }
+        }
+        countNumber--;
+    }
+    return arrayNumber;
+}
