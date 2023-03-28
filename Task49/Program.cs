3c76049b0179fc8db2fe337d592dcb4afeefc0df
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-28 18:36:00 +0600

    Task49 done

---
+++ Task49/Program.cs
@@ -0,0 +1,51 @@
+﻿// 49. Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+Console.Clear();
+
+int[,] myArr = CreateMatrixArr(3, 4, 1, 10);
+PrintArray(myArr);
+
+CheckToQutro(myArr);
+PrintArray(myArr);
+
+int[,] CreateMatrixArr(int countRows, int countColums, int min, int max)
+{
+    int[,] matrix = new int[countRows, countColums];
+    Random random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void CheckToQutro(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < array.GetLength(1); j += 2)
+        {
+            array[i, j] *= array[i, j];
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
