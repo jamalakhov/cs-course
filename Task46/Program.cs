ba66d7b3cccc60e01d90dcfe140c16d4be08d30c
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-28 18:22:23 +0600

    Task46

---
+++ Task46/Program.cs
@@ -0,0 +1,40 @@
+﻿// 46. Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+Console.Clear();
+
+int[,] myArr = CreateMatrixRndInt(4, 4, 1, 10);
+
+PrintArray(myArr);
+
+int[,] CreateMatrixRndInt(int countRows, int countColums, int min, int max)
+{
+    int[,] matrix = new int[countRows, countColums];
+    Random random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
