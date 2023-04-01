f648c6e49a6f8a8df4a5c04d5226f5cc0671c528
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-04-01 15:22:09 +0600

    Task54 done

---
+++ Task54/Program.cs
@@ -0,0 +1,72 @@
+﻿// 54. Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+
+int[,] newMatrix = CreateMatrixInt(4, 4, 1, 10);
+PrintMatrix(newMatrix);
+
+SortedElementEveryRowToDescending(newMatrix);
+PrintMatrix(newMatrix);
+
+//////////////////////////////////////////////////////////
+void SortedElementEveryRowToDescending(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        bool isSorted = false;
+
+        while (!isSorted)
+        {
+            isSorted = true;
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                if (matrix[i, j] < matrix[i, j + 1])
+                {
+                    isSorted = false;
+
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] CreateMatrixInt(int countRows, int countColumns, int min, int max)
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[countRows, countColumns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
