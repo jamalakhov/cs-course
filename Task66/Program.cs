ea6e69f724b972234fd2edca24a708dc8dbcd244
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-04-05 01:05:28 +0600

    Task66 done

---
+++ Task66/Program.cs
@@ -0,0 +1,40 @@
+﻿// 66. Задайте значения M и N.
+// Напишите программу, которая найдёт сумму натуральных элементов
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+
+int from = ReadConsole("Введите первое натуральное число: ");
+int to = ReadConsole("Введите второе натуральное число: ");
+
+if (!IsNatural(from) || !IsNatural(to))
+{
+    Console.WriteLine($"Отрезок [{from}, {to}] содержит ненатуральные числа");
+}
+else
+{
+    int resultSum = CalculateNaturalSumInRange(from, to);
+    Console.WriteLine(resultSum);
+}
+//////////////////////////////////////////////////////
+bool IsNatural(int num)
+{
+    return num > 0;
+}
+
+int CalculateNaturalSumInRange(int from, int to)
+{
+    if (from == to) return to;
+
+    return from < to
+                    ? CalculateNaturalSumInRange(from + 1, to) + from
+                    : CalculateNaturalSumInRange(from - 1, to) + from;
+}
+
+int ReadConsole(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
