a39e04c742ecf5e72e4ae659cec3e6fcc75b8f26
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-04-05 00:58:29 +0600

    Task63 done

---
+++ Task63/Program.cs
@@ -0,0 +1,25 @@
+﻿// 63. Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Clear();
+
+int number = ReadConsole("Введите целое положительное число: ");
+PrintNaturalNumber(number);
+
+//////////////////////////////////////////////////////////////
+void PrintNaturalNumber(int number)
+{
+    if (number > 0)
+    {
+        PrintNaturalNumber(number - 1);
+        Console.Write(number + " ");
+    }
+}
+
+int ReadConsole(string message)
+{
+    Console.Write(message);
+    return Conver.ToInt32(Console.ReadLine());
+}
